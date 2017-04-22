a46208c33cc695d8e777f769440e6d2fdb1f7d0e
David Ahmad <sa3904@nyu.edu>
2017-04-22 20:23:37 +0400

    Setup facebook webhook with verification

--- server.js
+++ server.js
@@ -5,17 +5,69 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const config = require('config');
+const crypto = require('crypto');
+
+// Get our API routes
+const webhook = require('./server/routes/webhook');
 
 const app = express();
 app.set('port', process.env.PORT || 5000);
 
+// Parsers for POST data
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Make dist folder public
 app.use(express.static(path.join(__dirname, 'dist')));
+
+// Setting config variables
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.get('appSecret');
+
+if (!(APP_SECRET)) {
+  console.error("Missing config value: APP_SECRET");
+  process.exit(1);
+}
+
+// Set our api routes
+app.use('/webhook', webhook);
 
 // Serve index.html page from the dist folder
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from 
+ * the App Dashboard, we can verify the signature that is sent with each 
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an 
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
 
 // Start server
 app.listen(app.get('port'), function() {
---
+++ server/routes/webhook.js
@@ -0,0 +1,53 @@
+/* jshint node: true, devel: true */
+"use strict";
+
+const express = require('express');
+const config = require('config');
+const request = require('request');
+const uuid = require('uuid');
+
+const router = express.Router();
+
+// Setting config variables
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.get('validationToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+
+// URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const SERVER_URL = (process.env.SERVER_URL) ?
+  (process.env.SERVER_URL) :
+  config.get('serverURL');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
+/*
+ * Use your own validation token. Check that the token used in the Webhook 
+ * setup is the same token used here.
+ *
+ */
+router.get('/', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook: successful");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+module.exports = router;
